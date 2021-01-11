b260265d43994fcc517519232f0f2778f68d80ae
unknown <emmanuelle.weimer@etu.unistra.fr>
2021-01-11 14:52:58 +0100

    messagerie

--- js/sw.js
+++ js/sw.js
@@ -1,7 +1,6 @@
-self.addEventListener('install', evt=>{
+self.addEventListener('install', listener: evt=>{
 	console.log(evt);
 });
-
-self.addEventListener('activate', evt => {
+self.addEventListener('activate', listener: evt=>{
 	console.log(evt);
-})
+});
