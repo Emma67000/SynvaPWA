9690a703324908385001b58d977c1d30a849b4dc
unknown <emmanuelle.weimer@etu.unistra.fr>
2021-01-15 09:36:08 +0100

    sw

---
+++ js/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', listener: evt=>{
+	console.log(evt);
+});
+self.addEventListener('activate', listener: evt=>{
+	console.log(evt);
+});
+self.addEventListener('fetch', evt=>{
+    if (!navigator.onLine){
+        evt.respondWith( new Response('pas de connexion internet'))
+    }
+    console.log(evt.request.url);
+})
